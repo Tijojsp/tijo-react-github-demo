007481079e97715ad7dbff508d0797b98c3991ff
tijojsp <tijojsp.may@gmail.com>
2024-10-13 20:50:19 -0400

    add workflow

---
+++ src/test/sample.test.js
@@ -0,0 +1,8 @@
+// tests/sample.test.js
+import { describe, it, expect } from 'vitest';
+
+describe('Sample Test', () => {
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+});
